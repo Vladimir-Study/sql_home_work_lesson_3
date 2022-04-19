882612a86c22c3eadd6a505623a96457b73773e0
Vladimir Pavlov <volodia.pavloff201343@gmail.com>
2022-04-19 23:17:53 +0300

    Внесены изменения в структуру БД по обозначенным замечаниям

--- home_work_lesson_3_main.sql
+++ home_work_lesson_3_main.sql
@@ -1,21 +1,19 @@
 create table if not exists Album (
 	id serial primary key,
 	name varchar(40) not null,
-	date_create date not null,
-	artist varchar(40) not null
+	date_create date not null
 );
 
 create table if not exists Track (
 	id serial primary key,
 	name varchar(40) not null,
-	time timestamp not null,
-	album integer references Album(id)
+	time float not null,
+	album integer not null references Album(id)
 );
 
 create table if not exists Artist (
 	id serial primary key,
-	name varchar(40) not null,
-	jenre varchar(40)
+	name varchar(40) not null
 );
 
 create table if not exists Jenre (
@@ -25,12 +23,14 @@ create table if not exists Jenre (
 
 create table if not exists ArtistJenre (
 	artist integer references Artist(id),
-	jenre integer references Jenre(id)
+	jenre integer references Jenre(id),
+	constraint aj_pk primary key (artist, jenre)
 );
 
 create table if not exists AlbumArtist (
 	album integer references Album(id),
-	artist integer references Artist(id)
+	artist integer references Artist(id),
+	constraint aa_pk primary key (album, artist)
 );
 
 create table if not exists Compilation (
@@ -41,5 +41,6 @@ create table if not exists Compilation (
 
 create table if not exists TrackCompilation (
 	track integer references Track(id),
-	compilation integer references Compilation(id)
+	compilation integer references Compilation(id),
+	constraint tc_pk primary key (track, compilation)
 );
